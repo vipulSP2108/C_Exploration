5ed20ba592f3362a70ac3c4d41051e3e47856af0
Vipul Sunil Patil <117573813+vipulSP2108@users.noreply.github.com>
2023-05-25 22:13:43 +0530

    Create main.c

---
+++ Join firstname and lastname-VIPulSP21/main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <string.h>
+
+int main(void){
+  // int num1, num2;
+  // printf("Enter length of your first name and secound name on seprate line\n");
+  // scanf("%d %d", &num1, &num2);
+  char first[5];
+  char secound[5];
+  printf("Enter First Name\n");
+  scanf("%s",&first[5]);
+  printf("Enter Secound Name\n");
+  scanf("%s",&secound[5]);
+  // printf("%s %s",first, secound);
+  // strcat(first, secound);
+}
