2b74a655f677e47197bca43e84dd3dc5a978bbfb
Vipul Sunil Patil <117573813+vipulSP2108@users.noreply.github.com>
2023-05-25 21:35:00 +0530

    Create main.c

---
+++ Find largest of four number-VIPulSP21/main.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main(void){
+  int num1, num2, num3, num4;
+  printf("Enter 4 number with sepration of space(' ')\n");
+  scanf("%d %d %d %d",&num1, &num2, &num3, &num4);
+  if (num1>=num2 && num1>=num3 && num1>=num4){
+    printf("%d",num1);
+  }
+  else if (num2>=num1 && num2>=num3 && num2>=num4){
+    printf("%d",num2);
+  }
+  else if (num3>=num1 && num3>=num2 && num3>=num4){
+    printf("%d",num3);
+  }
+  else{
+    printf("%d",num4);
+  }
+  return 0;
+}
