2c12e227040d802743bd8d8ff10e3524eba3c910
Vipul Sunil Patil <117573813+vipulSP2108@users.noreply.github.com>
2023-05-26 11:49:24 +0530

    Create main.c

---
+++ Product of two numbers-VIPulSP21 /main.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main(void){
+  int num1, num2;
+  printf("Enter number 1 and number 2 with space\n");
+  scanf("%d %d", &num1, &num2);
+  printf("%d",num1*num2);
+  return 0;
+}
